39e3338a50cb7c0f09af4e57ea83f420ef652658
Harika Chengelput <harika317@yahoo.com>
2018-03-15 16:44:16 +0000

    Updated the javascript code and added repeated code in functions.

--- scripts/app.js
+++ scripts/app.js
@@ -1,86 +1,117 @@
-const questionList = [
-  "Pick a word set that describes you well",
-  "What makes you feel strong?",
-  "Choose your weapon"
+const questionsList = [
+  {
+    questionNumber: 0,
+    questionText: "blank",
+    optionList: ["blank", "blank", "blank"]
+  },
+  {
+    questionNumber: 1,
+    questionText: "Pick a word set that describes you well",
+    optionList: [
+      "Strongest and protective",
+      "Intellectual and sarcastic",
+      "Tough and Serious"
+    ]
+  },
+
+  {
+    questionNumber: 2,
+    questionText: "Where do you want to get your energy from?",
+    optionList: [
+      "Energy from Sun",
+      "Peak Human Strength",
+      "A genius engineering mind"
+    ]
+  },
+
+  {
+    questionNumber: 3,
+    questionText: "Choose your weapon",
+    optionList: ["Yourself", "A strong hammer", "Armour and gadgets"]
+  }
 ];
-const optionlist1 = [
-  "Strongest and protective",
-  "Intellectual and sarcastic",
-  "Tough and Serious"
-];
-const optionlist2 = [
-  "Energy from Sun",
-  "Peak Human Strength",
-  "A genius engineering mind"
-];
-const optionlist3 = ["Yourself", "A strong hammer", "Armour and gadgets"];
+
 let questionCounter = 0;
 let chosenOptions = [];
+const totalQuestions = questionsList.length - 1;
 
-function loadQuestion1() {
-  questionCounter = 1;
-  document.querySelector("#questionText").textContent = questionList[0];
-  document.querySelector("#option1Text").textContent = optionlist1[0];
-  document.querySelector("#option2Text").textContent = optionlist1[1];
-  document.querySelector("#option3Text").textContent = optionlist1[2];
+function loadQuestion(qnumber) {
+  document.querySelector("#questionText").textContent =
+    questionsList[qnumber].questionText;
+  document.querySelector("#option1Text").textContent =
+    questionsList[qnumber].optionList[0];
+  document.querySelector("#option2Text").textContent =
+    questionsList[qnumber].optionList[1];
+  document.querySelector("#option3Text").textContent =
+    questionsList[qnumber].optionList[2];
+
+  if (totalQuestions === qnumber) {
+    document.querySelector(".nextButton").textContent = "Finish the game";
+  }
 }
-
-function loadQuestion2() {
-  questionCounter = 2;
-  document.querySelector("#questionText").textContent = questionList[1];
-  document.querySelector("#option1Text").textContent = optionlist2[0];
-  document.querySelector("#option2Text").textContent = optionlist2[1];
-  document.querySelector("#option3Text").textContent = optionlist2[2];
-  resetOptions();
-}
-
-function loadQuestion3() {
-  questionCounter = 3;
-  document.querySelector("#questionText").textContent = questionList[2];
-  document.querySelector("#option1Text").textContent = optionlist3[0];
-  document.querySelector("#option2Text").textContent = optionlist3[1];
-  document.querySelector("#option3Text").textContent = optionlist3[2];
-  document.querySelector(".nextButton").textContent = "Finish the Quiz";
-  resetOptions();
-}
-
-$(document).ready(function() {
-  console.log("ready!");
-  loadQuestion1();
-});
 
 document.querySelector(".nextButton").addEventListener("click", function() {
-  switch (questionCounter) {
-    case 1:
-      chosenOptions[0] = findSelectedOption();
-      console.log(chosenOptions);
-      loadQuestion2();
-      break;
-    case 2:
-      chosenOptions[1] = findSelectedOption();
-      console.log(chosenOptions);
-      loadQuestion3();
-      break;
-    case 3:
-      chosenOptions[2] = findSelectedOption();
-      console.log(chosenOptions);
+  if (document.querySelector(".nextButton").textContent === "Play again!") {
+    alert("YAyy Playing again");
+    playQuiz();
   }
+
+  if (document.querySelector("input[name=option]:checked") != null) {
+
+    if (questionCounter === totalQuestions) {
+      //last question process
+      chosenOptions.push(findSelectedOption());
+      showResult();
+      document.querySelector(".nextButton").textContent = "Play again!";
+    } 
+    
+    else {
+      questionCounter++;
+      chosenOptions.push(findSelectedOption());
+      console.log(chosenOptions);
+      loadQuestion(questionCounter);
+      resetOptions();
+    }
+
+  } 
+  
+  else {
+    alert("Please select an option");
+  }
+
 });
 
 function findSelectedOption() {
-  let selectedOption = "";
-  if (document.querySelector("#option1").checked) {
-    selectedOption = document.querySelector("#option1Text").textContent;
-  } else if (document.querySelector("#option2").checked) {
-    selectedOption = document.querySelector("#option2Text").textContent;
-  } else if (document.querySelector("#option3").checked) {
-    selectedOption = document.querySelector("#option3Text").textContent;
-  }
+  let selectedOption = document.querySelector("input[name=option]:checked")
+    .nextElementSibling.textContent;
   return selectedOption;
 }
 
 function resetOptions() {
-  document.querySelector("#option1").checked = false;
-  document.querySelector("#option2").checked = false;
-  document.querySelector("#option3").checked = false;
+  document.querySelector("input[name=option]:checked").checked = false;
 }
+
+function playQuiz() {
+  console.log("ready!");
+  questionCounter = 1;
+  chosenOptions = [];
+  loadQuestion(questionCounter);
+  document.querySelector(".nextButton").textContent = "Next";
+}
+
+function showResult() {
+  document.querySelector('.questionBox').childNodes.style.display = 'none';
+    document.querySelector('.questionBox').textContent = `
+  You have selected these options
+
+   ${chosenOptions}
+   
+   `;
+
+}
+
+
+
+$(document).ready(playQuiz());
+
+
